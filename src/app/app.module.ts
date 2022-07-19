eba4bd7a164d38066ea1d7c8169d29d1128233a0
mitjapotocin <mitja.potocin@gmail.com>
2022-07-19 22:07:21 +0200

    Refactor modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,43 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { MatCardModule } from '@angular/material/card';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatInputModule } from '@angular/material/input';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './components/nav/nav.component';
-import { HomeComponent } from './components/home/home.component';
-import { SearchInputComponent } from './components/search-input/search-input.component';
-import { CharacterListModule } from './components/character-list/character-list.module';
-import { DetailViewComponent } from './components/detail-view/detail-view.component';
-import { DetailComponent } from './components/detail-view/detail/detail.component';
 import { DetailViewModule } from './components/detail-view/detail-view.module';
+import { HomeModule } from './components/home/home.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    HomeComponent,
-    SearchInputComponent
   ],
   imports: [
-    CharacterListModule,
     DetailViewModule,
     HttpClientModule,
     BrowserModule,
-    MatCardModule,
-    MatInputModule,
-    MatIconModule,
-    MatButtonModule,
-    MatSnackBarModule,
+    HomeModule,
     FormsModule,
-    MatPaginatorModule,
     AppRoutingModule
   ],
   providers: [],
--- src/app/components/home/home.module.ts
+++ src/app/components/home/home.module.ts
@@ -4,32 +4,38 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { CharacterCardComponent } from './character-card/character-card.component';
-import { CharacterListComponent } from './character-list.component';
+import { CharacterListComponent } from './character-list/character-list.component';
 import { AppRoutingModule } from 'src/app/app-routing.module';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-
-const components = [
-  CharacterListComponent,
-  CharacterCardComponent
-]
+import { HomeComponent } from './home.component';
+import { SearchInputComponent } from '../search-input/search-input.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 @NgModule({
   declarations: [
-    ...components
+    CharacterListComponent,
+    CharacterCardComponent,
+    HomeComponent,
+    SearchInputComponent
   ],
   imports: [
     MatCardModule,
     MatProgressSpinnerModule,
+    MatInputModule,
     BrowserAnimationsModule,
     MatButtonModule,
+    MatPaginatorModule,
     MatIconModule,
+    MatSnackBarModule,
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
-  exports: [
-    ...components
-  ],
+  exports: [],
   providers: [],
 })
-export class CharacterListModule { }
+export class HomeModule { }
